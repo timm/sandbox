314ced06d1b12f57f43ca3f0e0b16552deb60150
Tim Menzies <tim.menzies@gmail.com>
2017-10-25 09:49:31 -0400

    sada

--- lisp/lib.lisp
+++ lisp/lib.lisp
@@ -5,3 +5,15 @@
 
 (deftest nchars? ()
   (test (nchars 3 ".") "..."))
+
+
+(defun make-accessor-field (name form)
+    `(,name
+           :initarg  ,(intern (symbol-name name) "KEYWORD")
+	       :initform ,form
+	           :accessor ,name))
+
+(defmacro defclass-with-accessors (name slot-descriptions)
+    `(defclass ,name ()
+            ,(loop for (slot-name form) in slot-descriptions
+		               collect (make-accessor-field slot-name form))))
