6eb56e7fd882f71792b9e9122d1bc84f55850907
Tim Menzies <tim.menzies@gmail.com>
2017-11-01 18:57:11 -0400

    more adding

--- lisp/fft.lisp
+++ lisp/fft.lisp
@@ -4,71 +4,87 @@
 #|
 Here some more
 |#
-(defthing col
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; col
+
+(defthing col thing
   (txt "") (pos 0) (n 0) (w 1))
 
+(defmethod add1((x col) y)
+  (assert nil () "add1 should be implemented by subclass"))
 
+(defmethod norm1((x col) y)
+  (assert nil () "norm1 should be implemented by subclass"))
+
+;; vvariance(samples):
+;;   M := 0
+;;   S := 0
+;;   for k from 1 to N:
+;;     x := samples[k]
+;;     oldM := M
+;;     M := M + (x-M)/k
+;;     S := S + (x-M)*(x-oldM)
+;; return S/(N-1)
 
 (defmethod add ((x col) y)
-  (when (not (eql y #\?))
-    (incf (slot-value x) 'n)
-    (add1 y)
+  (with-slots (n) x
+    (when (not (eql y #\?))
+      (incf n)
+      (add1 x y))
     y))
 
-(defmethod adds ((x col) ys) (dolist (y ys y) (add y)))
+(defmethod adds ((x col) ys)
+  (dolist (y ys y)
+    (add x y))
+  x)
 
 (defmethod norm ((x col) y)
   (if (eql y #\?)
       y
     (norm1 x y)))
 
-(defklass num col
-  (mu 0) (m2 0)
-  (lo most-positive-fixnum)
-  (hi most-negative-fixnum))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; num
 
+(defthing num col
+  (mu 0) (m2 0) (sd 0)
+  (lo most-positive-fixnum)
+  (hi most-negative-fixnum)) 
+
+(defmethod print-object ((x num) src)
+  (with-slots (n pos txt w mu m2 lo hi) x
+    (format src "~a"
+      `((n  . ,n) (pos . ,pos) (txt . ,txt) (w  . ,w)
+        (mu . ,mu) (m2 . ,m2)  (lo  . ,lo)  (hi . ,hi)))))
+  
 (defmethod add1 ((x num) y)
-  (with-slots  (hi lo n mu m2) x
+  (with-slots (hi lo n mu m2 sd) x
     (let ((delta (- y mu)))
       (setf lo (min lo y)
-            hi (max hi y)
-            mu (+ mu (/ delta n))
-            m2 (+ m2 (* delta (- x mu))))
+             hi (max hi y)
+             mu (+ mu (/ delta n))
+             m2 (+ m2 (* delta (- y mu))))
       (if (> n 1)
-          (setf sd (/ m2 (- n 1)))))))
-          
+          (setf sd (sqrt (/ m2 (- n 1))))))))
 
+(defmethod any ((x num) &aux x1 x2 (w 1))
+  (while (>= w 1)
+      (setf x1 (- (* 2 (randf) 1))
+            x2 (- (* 2 (randf) 1))
+            w  (+ (* x1 x1) (* x2 x2))))
+  (with-slots (mu sd) x
+    (+ mu (* sd w x1 (sqrt (/ (* -2 (log w)) w))))))
 
-(let ((n 0))
-  (defstruct num name pos (n 0) (mu 0) (m2 0) (id (incf n)))
-  (defstruct sym name pos (id (incf n))))
+(deftest col? ()
+  (let ((x (make-instance 'num)))
+    (adds x '(9 2 5 4 12 7 8 11 9 3 7 4 12 5 4 10 9 6 9 4))
+    (test 3.06  (r2 (slot-value x 'sd)))
+    (test 7.0   (r0 (slot-value x 'm)))))
+        
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; deftable
 
-(defmethod add ((i num) y)
-  (with-slots (n mu m2) i
-    (incf n)))
-    
-  
-(defstruct cols (all)  (nums) (syms)) 
-(defstruct tbl name rows
-           (cols)
-           (xy (make-cols))   
-           (x  (make-cols))
-           (y  (make-cols)) 
-           (klasses) 
-           (less)
-           (more))
-
-; # More Test
-;
-; and more
-; asas
-
-;(defmethod print-object ((x tbl) str)
- ; "Change the print method for a hash."
-  ;(format str "(tbl ~a)" (length (tbl-rows x))))
-
-                                        ; # FFT
-; Stuff to tell
 (defun deftable1 (name cols rows)
   (let ((tb (make-tbl :name name :cols cols)))
     (doitems (col pos cols tb)
--- lisp/lib.lisp
+++ lisp/lib.lisp
@@ -10,6 +10,21 @@
        (incf ,n)
        ,@body)))
 
+(defmacro while (test &body body)
+  `(do ()
+       ((not ,test))
+     ,@body))
+
+(defmacro slots (obj &rest names)
+  `(mapcar #'(lambda (name) (cons name (slot-value ,obj name))) ',names))
+  
+(defun round-to (number precision &optional (what #'round))
+    (let ((div (expt 10 precision)))
+      (float (/ (funcall what (* number div)) div))))
+
+(defun r2 (n) (round-to n 2))
+
+(defun r0 (n) (round-to n 0))
 
 ;;;; Simple access/update to recursive slots in LISP
 ;;;; Tested on defstructs. Should also work on instances.
@@ -23,6 +38,10 @@
       (change f (slot-value obj (car slots)) (cdr slots))
       (setf (slot-value obj (car slots))
             (funcall f (slot-value obj (car slots))))))
+
+(defmacro dochange ((slot obj &rest slots) &rest body)
+  `(changex #'(lambda (,slot) ,@body)
+            ,obj ',slots))
 
 (defmacro ? (obj first-slot &rest more-slots)
   "From https://goo.gl/dqnmvH.
@@ -53,18 +72,28 @@
     tmp))
 
 
-(defun make-accessor-field (name form)
+(defun defslot  (name form)
   `(,name
     :initarg  ,(intern (symbol-name name) "KEYWORD")
     :initform ,form
     :accessor ,name))
 
-(defmacro defthing  (name &rest slot-descriptions)
-  `(defclass ,name ()
-     ,(loop for (slot-name form) in slot-descriptions
-         collect (make-accessor-field slot-name form))))
+(defclass thing () ())
 
-(defmacro defklass (name parent &rest slot-descriptions)
-  `(defclass ,name (,parent)
-     ,(loop for (slot-name form) in slot-descriptions
-         collect (make-accessor-field slot-name form))))
+(defmacro  defthing (x parent &rest slots)
+  `(defclass ,x (,parent)
+     ,(loop for (x form) in slots collect (defslot x form))))
+
+(let* ((seed0      10013)
+       (seed       seed0)
+       (multiplier 16807.0d0)
+       (modulus    2147483647.0d0))
+  (defun reset-seed ()  (setf seed seed0))
+  (defun randf      (&optional (n 1)) (* n (- 1.0d0 (park-miller-randomizer))))
+  (defun randi      (&optional (n 1)) (floor (* n (/ (randf 1000.0) 1000))))
+  (defun park-miller-randomizer ()
+    "cycle= 2,147,483,646 numbers"
+    (setf seed (mod (* multiplier seed) modulus))
+    (/ seed modulus))
+)
+
