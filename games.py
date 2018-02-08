0b8fd51eec595c230ab80d13c41c7b7bfec2a102
Tim Menzies <tim.menzies@gmail.com>
2018-02-08 11:55:09 -0500

    gamine

--- games.py
+++ games.py
@@ -41,15 +41,13 @@ class MyGame(Machine):
   def run(i):
     payload = o(whatever)
     try:
-      i.walk(payload, i.start) 
-    except Drama as event: #can check for super class? https://goo.gl/H3jz7x
-      # handle event
+      state = i.start
+      while state:
+        state = state.step(payload)
+    except Drama as event : #can check for super class? https://goo.gl/H3jz7x
+      1 # handle event
     finally: # always runs, exception of no
       return payload
-  # -----------------------
-  def walk(i, payload, state):
-    while state:
-       state = state.step(payload)
 
 """
 Belifs desires intentions https://goo.gl/PJygJy
@@ -58,12 +56,44 @@ Welcome to high church AI
 class State:
   def __init__(i,name,machine):
     i.name, i.machine = name,machine
-    beliefs=[] # if simple symbols, insert here. if complex structure,
-               # make it a state
-    intentions=[] # short term stuff. queries and operations on this state
-    desires=[]    # longer terms stuff. when  
+    i.beliefs=[] # working memory of things you ahve worked out
+                 # if simple symbols, insert here. if complex structure, 
+                 # make it a state and go to that state
+    i.intentions=[] # short term stuff. queries and operations on this state
+    i.desires=[]    # longer terms stuff. when  
+  """
+  Make intentions a list of gaurds which, if satisfied, return a list
+  of actions (messages to this state)
+  Make desires a a list of messages to send to the next state, so
+  some meory can pass from here to there
+  """
+  def step(i,payload):
+    "match, select, act"
+    todo = i.match(payload)
+    if todo:
+      command = i.select(todo)
+      return i.act([payload, command)
+    return i
 
-# make intentions a list of gaurds which, if satisfied, return a list 
-# of actions (messages to this state) 
-# make desires a a list of messages to send to the next state, so
-# some meory can pass from here to there
+  def match(i,payload):
+    out=[]
+    for j in i.intentions: #?? replace shuffle with priority?
+      todo,there =  j.gaurd(i,payload):
+      if todo:
+        out += [(j.__name__,todo,there)]
+    return out
+
+  def select(i, candidates):
+    return random.choice(matched)
+
+  def act(i, payload, (what,actions, there)):
+    print("now",what)
+    [action(payload) for action in actions]
+    i.onExit()
+    there.onEntry()
+    return there
+
+  def exampleGaurd(i,payload):
+    "dance in place"
+    if payload.happy:
+      return [i.machine.grin, i.machine.dance], i
