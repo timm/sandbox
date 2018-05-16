d255521ef311e16b010db88c40177449a1cfa915
Tim Menzies <timm@Timothys-MacBook-Air-2.local>
2018-05-16 00:37:27 -0400

    as

--- py/cocomo/hyperband.py
+++ py/cocomo/hyperband.py
@@ -1,3 +1,6 @@
+import math,time
+from random import random as r
+from random import choice as any
 import auto 
 
 def bands(lst,n=3):
@@ -8,22 +11,42 @@ def bands(lst,n=3):
     i /= n
     r *= n
 
-def kv(d,keys=None, decimals=3):
+def same(x): return x
+def first(l): return l[0]
+def second(l): return l[0]
+def last(l): return l[-1]
+
+def kv(d,keys=None, decimals=None):
    "print dictionary, in key sort order"
+   decimals = decimals or THE.decimals
    keys = keys or sorted(list(d.keys()))
    pretty = lambda x: round(x,decimals) if isinstance(x,float) else x
-   return '<'+', '.join(['%s: %s' % (k, pretty(d[k])) for k in keys)  + '>'
+   return '{'+', '.join(['%s: %s' % (k, pretty(d[k]))for k in keys])+'}'
 
 class o(object):
   "Javascript envy. Now 'o' is like a JS object."
-  def __init__(i, **l)    : i.__vals().update(l)
+  def __init__(i, **kv)   : i.__dict__.update(kv)
   def __getitem__(i, k)   : return i.__dict__[k]
-  def __setitem__(i, k, v): return i.__dict__[k] = v
-  def __repr__(i):
-    return i.__class__.__name__ + kv(i.__dict__,i.__keys())
-  def __keys(j): 
-    return [k for k in sorted(i.__dict__.)keys()) if not k[0] is "_"]
+  def __setitem__(i, k, v): i.__dict__[k] = v
+  def __repr__(i): 
+    return i.__class__.__name__ + kv(i.__dict__, i._has())
+  def _has(i): 
+    return [k for k in sorted(i.__dict__.keys()) if k[0] != "_"]
 
+def timeit(f):
+  t1=time.perf_counter()
+  f()
+  return time.perf_counter() - t1
+
+#-------------
+THE=o(decimals = 3,
+      doubting = 1.05,
+      cohen    = 0.2,
+      power    = 0.5,
+      few      = 20
+     )
+
+#-------------
 class Row(o):
   def __init__(i,x,y,w): 
     i.w,i.x,i.y,i.dom = w,x,y,0
@@ -70,18 +93,62 @@ class Table:
     for x in rows[:10]: print("<",x.y, x.dom)
     for x in rows[-10:]: print(">",x.y, x.dom)
 
-class Rules:
-  def __init__(i,data):
-    t = Table(data[0], data[1:])
-
 def nodes(t):
   if t:
+    if t.left:
+      for t1 in nodes(t.left):
+        yield t1
+    if t.right:
+      for t2 in nodes(t.right):
+        yield t2
     yield t
-    for kid in [t.left, t.right]:
-      for z in nodes(kid):
-        yield z
 
-# needs to 
+class Thing(o):
+  def __init__(i, inits=[],f=lambda z:z):
+    i.locals()
+    i.n, i._f = 0, f
+    #[i + x for x in inits]
+    [i+x for x in inits]
+  def __add__(i,x):
+    if x != None:
+      i.n += 1
+      print("!!",x)
+      y=i._f(x)
+      print(">>",x,y)
+      i._add( i._f(x) )
+  def simpler(i):
+    return i.doubt > THE.doubting * (
+                           j.doubt() * j.n/i.n + 
+                           k.doubt() * k.n/i.n ) 
+
+class Num(Thing):
+  def locals(i): i.mu = i.m2 =  0
+  def doubt(i): return i.sd()
+  def _add(i,x):
+    print(x)
+    delta  = x - i.mu
+    i.mu  += delta/i.n
+    i.m2  += delta*(x - i.mu)
+  def sd(i): return (i.m2/(i.n - 1))**0.5
+  def __repr__(i):
+    return 'Num'+kv(dict(mu=i.mu, sd=i.sd(), n=i.n))
+
+class Sym(Thing):
+  def locals(i): i.seen, i._ent = {}, None
+  def doubt(i): return i.ent()
+  def _add(i,x):
+    i.seen[x] = i.seen.get(x,0) + 1
+    i._ent = None
+  def ent(i):
+    if i._ent == None:
+      i._ent = 0
+      for _,v in i.seen.items():    
+        p= v/i.n
+        i._ent -= p*math.log(p,2)
+    return i._ent
+  def __repr__(i):
+    return 'Num'+kv(dict(seen=i.seen, ent=i.ent(), n=i.n))
+
 def splits(lst, epsilon=None, few=None, x=same, y=same):
   def val(j): return x( lst[j] )
   def worker(lo, hi, parent, lvl):
@@ -100,19 +167,31 @@ def splits(lst, epsilon=None, few=None, x=same, y=same):
           left  = worker(lo,  m, node, lvl+1)
           right = worker(m,  hi, node, lvl+1)
           if left.cut and right.cut:
-            cuts += [m]
+            cuts.append(m)
             node.left = left
             node.right = right
     return node
   # main ------------------
   cuts    = []
+  print(1,x)
+  
   epsilon = epsilon or Num(inits=lst,f=x).sd()*THE.cohen
+  print(2)
   few     = few     or len(lst)**THE.power
   few     = max(few,THE.few)
   lst     = sorted(lst, key=x)
   return cuts,worker( 0, len(lst), None, 0) 
- 
-Rules(auto.data)
+
+def showt(t, tab="|..", pre="",lvl=0,val=lambda z: ""):
+  if t:
+    print( tab*lvl + pre + str( val(t) ))
+    showt(t.left,  tab, "< ", lvl+1, val)
+    showt(t.right, tab, "> ", lvl+1, val)
+
+t=splits(auto.data[1:],x=second,y=last)
+showt(t)
+
+#Table(auto.data[0], auto.data[1:])
 
 # #rows have id and objs and __lt__
 # def chunkify(lst):
@@ -121,7 +200,7 @@ Rules(auto.data)
 #     yield lst[a:a+c]
 #     a += c
 # 
-# def best(d, nth): 
+## def best(d, nth): 
 #   some = int( len(d.keys()) * nth )
 #   return sorted(list(d.values))[-some:]
 # 
