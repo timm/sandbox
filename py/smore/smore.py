b7806365181d55204439b80a282389cc2bf44121
Tim Menzies <timm@Timothys-MacBook-Air-2.local>
2018-05-16 23:53:08 -0400

    update

--- py/smore/smore.py
+++ py/smore/smore.py
@@ -1,28 +1,50 @@
-import math,time
+banner="""
+ ___ _ __ ___   ___  _ __ ___ 
+/ __| '_ ` _ \ / _ \| '__/ _ \ 
+\__ \ | | | | | (_) | | |  __/
+|___/_| |_| |_|\___/|_|  \___|
+
+(simple multi-objective rule engine)
+"""
+import getopt,sys,re,csv,math,time
 from random import random as r, choice as any, seed as seed
-import auto 
 
 class o(object):
   "Javascript envy. Now 'o' is like a JS object."
   def __init__(i, **kv):    i.__dict__.update(kv)
-  def __getitem__(i, k):    return i.__dict__[k]
   def __setitem__(i, k, v): i.__dict__[k] = v
-  def __repr__(i): return i.__class__.__name__ + kv(i.__dict__, i._has())
-  def _has(i):     return [k for k in sorted(i.__dict__.keys()) if k[0] != "_"]
+  def __getitem__(i, k): return i.__dict__[k]
+  def keys(i):           return i.__dict__.keys()
+  def __repr__(i):       return i.__class__.__name__ + kv(i.__dict__, i._has())
+  def _has(i):           return [k for k in sorted(i.__dict__.keys()) if k[0] != "_"]
 
-#-------------------------------------
-THE = o(decimals   = 3,
-        undoutable = 1.05,
-        cohen      = 0.2,
-        power      = 0.5,
-        few        = 4
+THE = o(   
+           cohen      = 0.2,
+           DATA       = 'auto.csv',
+           decimals   = 3,
+           few        = 4,
+           MAIN       = 'SMORE',
+           power      = 0.5,
+           undoutable = 1.05
        )
 
-#-------------------------------------
+#------------------------------------
+def demo(f=None, act=None, all=[]):
+  if     f: all += [f]
+  elif act:
+    for one in all:
+      if one.__name__ == act: one()
+  else: [f() for f in all]
+  return f
+
+@demo
+def SMORE(): print(banner)
+
 def same(x): return x
 def first(l): return l[0]
 def second(l): return l[1]
 def last(l): return l[-1]
+def sym(x): return x
 
 def kv(d,keys=None, decimals=None):
    "print dictionary, in key sort order"
@@ -35,6 +57,53 @@ def timeit(f):
   t1=time.perf_counter()
   f()
   return time.perf_counter() - t1
+
+def csv(file, doomed=r'([\n\r\t]|#.*)', sep=",", skip="?"):
+  "World's smallest csv reader?"
+  use,rows,ako,hdr = [],[],[],None
+  with open(file) as fs:
+    for n,lst in enumerate(fs):
+      lst = re.sub(doomed, "", lst)
+      row = [z.strip() for z in lst.split(sep)]
+      if len(row) > 0:
+        use = use or [n for n,x in enumerate(row) if x[0] != skip]
+        row = [row[n] for n in use]
+        if n==0:
+          hdr     = row
+          objs    = [n for n,x in enumerate(hdr) if x[0] in '<>']
+          decs    = [n for n,x in enumerate(hdr) if not n in objs]
+          weights = [-1 if hdr[n][0]=="<" else 1 for n in objs]
+          ako     = [float if row[n][0] in "$<>" else sym for n in use]
+        if n > 0:
+          row     = [x if x[0]==skip else p(x) for x,p in zip(row,ako)]
+          rows   += [row]
+    return o(file=file, ako=ako, head=hdr, objs=objs, 
+             decs=decs, weights=weights, rows=rows)
+
+def main(calling, d, argv=None):
+  "Configure command line parser from keys of dictonary 'd'"
+  argv = argv or sys.argv[1:]
+  opts = 'h%s' % ''.join(['%s:' % k[0] for k in d.keys()])
+  oops = lambda x="": print(x) or sys.exit(2)
+  try:
+    com, args = getopt.getopt(argv,opts)
+    for opt, arg in com:
+      if opt == '-h':
+        print(calling + " -h", end="")
+        for k in THE.keys(): 
+          print(" -%s %s:%s" % (k[0],k,d[k]),sep="",end="")
+        oops()
+      else:
+        for k in d.keys():
+          if opt[1]==k[0]:
+            try: new= int(arg)
+            except: 
+              try: new= float(arg)
+              except: new= arg
+            if type(d[k]) != type(new): 
+              oops('%s expected %s' % (opt,type(d[k]).__name__))
+            d[k] = new
+  except getopt.GetoptError as err: oops(err)
 
 #-------------
 class Row(o):
@@ -83,16 +152,6 @@ class Table:
     for x in rows[:10]: print("<",x.y, x.dom)
     for x in rows[-10:]: print(">",x.y, x.dom)
 
-def nodes(t):
-  if t:
-    if t.left:
-      for t1 in nodes(t.left):
-        yield t1
-    if t.right:
-      for t2 in nodes(t.right):
-        yield t2
-    yield t
-
 class Thing(o):
   def __init__(i, inits=[],f=lambda z:z):
     i.locals()
@@ -140,16 +199,32 @@ class Sym(Thing):
   def __repr__(i):
     return 'Sym'+kv(dict(seen=i.seen, ent=i.ent(), n=i.n))
 
+def tree(t):
+  if t:
+    yield t
+    if t.left:
+      for t1 in tree(t.left): yield t1
+    if t.right:
+      for t2 in tree(t.right): yield t2
+
+def supertree(t):
+  if t:
+    yield t
+    if t._up:
+      for t1 in supertree(t._up): yield t1
+
+def bottomUp(t):
+  return sorted( [t for t in tree(t)], 
+                 key= lambda z:z.level, 
+                 reverse= True)
+
 def grow(lst, epsilon=None, few=None, x=same, y=same):
   "returns nil if nothing"
   def makeNode(lst, lvl=0, up=None):
-    return o(x= Num( lst, f=x ),
-             y= Num( lst, f=y ),
+    return o(x= Num( lst, f=x ), y= Num( lst, f=y ),
              level= lvl,
              _up  = up, 
-             simplifies=False,
-             left = None, 
-             right= None) 
+             simplifies=False, left = None, right= None) 
   def X(j): 
     return x( lst[j] )
   def mid(lo,hi):
@@ -168,14 +243,11 @@ def grow(lst, epsilon=None, few=None, x=same, y=same):
              node.left = recurse(lo=lo,  hi=m,  up=node,  lvl=lvl+1)
              node.right= recurse(lo=m,   hi=hi, up=node, lvl=lvl+1)
     return node
-  # main ------------------
   lst     = sorted(lst, key=x)
   root    = makeNode(lst)
   epsilon = epsilon or root.x.sd()*THE.cohen
   few     = max(few or len(lst)**THE.power, THE.few)
-  print(dict(e=epsilon, few=few,bins=int(len(lst)/few)))
   return recurse(up=root)
-  #return root
 
 def showt(t, tab="|.. ", pre="",lvl=0,val=lambda z: ""):
   if t:
@@ -186,85 +258,35 @@ def showt(t, tab="|.. ", pre="",lvl=0,val=lambda z: ""):
     if t.right:
       showt(t.right, tab, "> ", lvl+1, val)
 
-
 def _grow(X=same,Y=same,N=1000):
-  def _grow1(z): return [X(z),Y(z)]
+  def show(z): return [X(z),Y(z)]
   seed(1)
   _show = lambda z:  '%s to %s (%.1f) # %s' %(
                        z.x.lo, z.x.hi, z.x.mu,z.x.n)
   print("\n--------------------------")
-  tree  = grow( [_grow1(int(100*r())) for _ in range(N)],
-                     x=first,
-                     y=last)
+  tree = grow( [show(int(100*r())) for _ in range(N)],
+               x=first,
+               y=last)
   showt( tree, val=_show )
+  return tree
 
-def xx(x):
+@demo
+def GROW0(): 
+  _grow()
+
+@demo
+def GROW1(): 
+  _grow(X=lambda x : 0 if x <40 else x,N=256)
+
+@demo
+def GROW2():
+  def xx(x):
     if x < 10: return x
     if x < 40: return 40
     if x < 70: return x
     return 70
+  _grow(X=xx,N=256)
 
-_grow()
-_grow(X=lambda x : 0 if x <40 else x,N=256)
-_grow(X=xx,N=256)
-
-#Table(auto.data[0], auto.data[1:])
-
-# #rows have id and objs and __lt__
-# def chunkify(lst):
-#   a,b,c = 0, len(lst), int(len(lst))/chunks
-#   while a < b:
-#     yield lst[a:a+c]
-#     a += c
-# 
-## def best(d, nth): 
-#   some = int( len(d.keys()) * nth )
-#   return sorted(list(d.values))[-some:]
-# 
-# # use Dom(lst);sorted(lst) and you'll e god
-# # level defaults to zerp
-# def dombig(lst, ws, nth=3, p=0.5, small=100)
-#   def chunkify(lst):
-#     a,b,c = 0, len(lst), int(len(lst))/chunks
-#     while a < b:
-#       yield lst[a:a+c]
-#       a += c
-# 
-#     return sorted(list(d.values))[-some:]
-# 
-#   def dom(x,y):
-#     s1, s2, n, z = 0, 0, len(x.objs), 10**-32
-#     for a,b,w,lo,hi in zip(x.objs,y.objs,ws,lows,highs):
-#         a   = (a - lo) / (hi - lo + z)
-#         b   = (b - lo) / (hi - lo + z)
-#         s1 -= e**( w * (a-b)/n )
-#         s2 -= e**( w * (b-a)/n )
-#     return s1/n < s2/n
-# 
-#   def doms(lst, doms):
-#     for x in lst:
-#       for y in lst:
-#         if dom(x, y):
-#           doms[x.id] = doms[x.id] if x.id in doms else x
-#           one.dom += 1
-#     return doms
-# 
-#   def step(pop, level):
-#     for chunk in chunkify( pop ) :
-#       for one in bore( doms(chunk,{}), 0.5)
-#         one.level = level
-#         yield one
-# 
-#   lows,highs = ranges(lst, {}, {})
-#   if len(lst) < small:
-#     doms( lst,{} )
-#     for one in lst:
-#       one.level = one.dom
-#   else:
-#     chunks = len(lst)**0.5
-#     level = 0
-#     pop   = random.shuffle(lst)
-#     while len(pop) > chunks:
-#       level += 1
-#       pop = [ one for one in step(pop,level) ]
-#   return sorted(lst, key=lambda z: z.level)
+if __name__ == "__main__":
+   main("pypy3 smore.py",THE)
+   demo(act=THE.MAIN)
